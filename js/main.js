3b485517e1d7645e31daae565ee2dc27903daa6b
Light068068 <106878508+Light068068@users.noreply.github.com>
2022-06-09 15:09:13 +0530

    .

--- js/main.js
+++ js/main.js
@@ -29,3 +29,49 @@ menubtn.addEventListener("click", () => {
 //     ico.classList.toggle("deactive");
 //   }
 // else{}
+
+// start typeWriter by pure JS
+const txtElement = document.getElementById("typewriter");
+const typed = ["This is the Best Blood Donation portal."];
+let index = 0;
+let typer = 0;
+let currentTyped = [];
+let isDeleting = false;
+let isEnd = false;
+function loop() {
+  isEnd = false;
+  txtElement.innerHTML = currentTyped.join("");
+
+  if (index < typed.length) {
+    if (!isDeleting && typer <= typed[index].length) {
+      currentTyped.push(typed[index][typer]);
+      typer++;
+      txtElement.innerHTML = currentTyped.join("");
+    }
+
+    if (isDeleting && typer <= typed[index].length) {
+      currentTyped.pop(typed[index][typer]);
+      typer--;
+      txtElement.innerHTML = currentTyped.join("");
+    }
+    if (typer == typed[index].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && typer === 0) {
+      currentTyped = [];
+      isDeleting = false;
+      index++;
+      if (index == typed.length) {
+        index = 0;
+      }
+    }
+  }
+  const typeSpeed = Math.random() * (100 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 100;
+  const time = isEnd ? 1000 : isDeleting ? typeSpeed : normalSpeed;
+  setTimeout(loop, time);
+}
+
+loop();
