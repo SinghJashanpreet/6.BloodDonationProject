c408903839c530cfc6cb430b54a1766de5185ae7
SinghJashanpreet <87263916+SinghJashanpreet@users.noreply.github.com>
2022-06-11 11:03:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-var source = "audio.mp3"
+var source = "audio.mp3";
 var audio = document.createElement("audio");
 //
 audio.autoplay = true;
 //
-audio.load()
-audio.addEventListener("load", function() { 
-    audio.play(); 
-}, true);
+audio.load();
+audio.addEventListener(
+  "load",
+  function () {
+    audio.play();
+  },
+  true
+);
 audio.src = source;
-
-
 
 window.addEventListener("scroll", function () {
   const nav = document.querySelector(".navbar");
@@ -20,28 +22,21 @@ const menubtn = document.getElementById("menubtn");
 const menu = document.getElementById("menu");
 const menu_right = document.getElementById("menu_right");
 const logo = document.getElementById("logo");
-const dropbtn= document.getElementById("dropbtn");
-const dropbtn1= document.getElementById("dropbtn1");
+const dropbtn = document.getElementById("dropbtn");
+const dropbtn1 = document.getElementById("dropbtn1");
 
 menubtn.addEventListener("click", () => {
-    
-  //  if (menu_right.style.position="fixed") {
-    menu.style.display = "block";
-    menu_right.classList.toggle("active");
-      ico.classList.toggle("active");
-  // } else {
-  //   menu.style.display = "block";
-  //   menu_right.classList.toggle("active");
-  //     ico.classList.toggle("active");
-    
-  // }
+
+  menu.style.display = "block";
+  menu_right.classList.toggle("active");
+  ico.classList.toggle("active");
+
 
   if (logo.style.display === "none") {
     logo.style.display = "block";
   } else {
     logo.style.display = "none";
   }
-
 
   if (dropbtn.style.display === "none") {
     dropbtn.style.display = "inline-block";
@@ -54,48 +49,31 @@ menubtn.addEventListener("click", () => {
   } else {
     dropbtn1.style.display = "none";
   }
-
-
 });
 
-// var html = document.getElementsByClassName("html1");
-// if(window.innerWidth<=830)
-// {
-//   html.innerHTML="<h1>jashan</h1>";
-// }
-// else{
-// document.innerHTML
-// }
+const filteritem = document.querySelector(".filter-projects");
+const filterbox = document.querySelectorAll(".project-box");
 
-
-  //for active class
-  const filteritem = document.querySelector(".filter-projects");
-  const filterbox = document.querySelectorAll(".project-box");
-  
-  window.onload = () => {
-    //once window loaded
-    filteritem.onclick = (selectedItem) => {
-      if (selectedItem.target.classList.contains("item")) {
-        filteritem.querySelector(".f-active").classList.remove("f-active");
-        selectedItem.target.classList.add("f-active");
-        let filtername = selectedItem.target.getAttribute("data-filter");
-        filterbox.forEach((img) => {
-          let filterimgs = img.getAttribute("data-filter");
-          if (filterimgs == filtername || filtername == "all") {
-            img.classList.remove("hide");
-            img.classList.add("show");
-          } else {
-            img.classList.add("hide");
-            img.classList.remove("show");
-          }
-        });
-      }
-    };
+window.onload = () => {
+  //once window loaded
+  filteritem.onclick = (selectedItem) => {
+    if (selectedItem.target.classList.contains("item")) {
+      filteritem.querySelector(".f-active").classList.remove("f-active");
+      selectedItem.target.classList.add("f-active");
+      let filtername = selectedItem.target.getAttribute("data-filter");
+      filterbox.forEach((img) => {
+        let filterimgs = img.getAttribute("data-filter");
+        if (filterimgs == filtername || filtername == "all") {
+          img.classList.remove("hide");
+          img.classList.add("show");
+        } else {
+          img.classList.add("hide");
+          img.classList.remove("show");
+        }
+      });
+    }
   };
-  
-
-
-
+};
 
 // start typeWriter by pure JS
 const txtElement = document.getElementById("typewriter");
@@ -140,9 +118,5 @@ function loop() {
   const time = isEnd ? 1000 : isDeleting ? typeSpeed : normalSpeed;
   setTimeout(loop, time);
 }
-
-
-
-
 
 loop();
